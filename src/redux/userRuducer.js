961fb20b94f2793598a217eb43fe6650d0bfa7b7
stephenkati <stephenkatuli88@gmail.com>
2023-07-21 22:38:14 +0300

    Create userReducer

---
+++ src/redux/userRuducer.js
@@ -0,0 +1,87 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+const createUser =  createAsyncThunk(
+  'user/register',
+  async (registrationDetails, thunkAPI) => {
+    try {
+      const response = await axios.post(
+        '/api/v1/users',
+        {registrationDetails},
+        {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+      
+      console.log(response.data)
+      return response.data;
+
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.response.data)
+    }
+  }
+);
+
+export const logInUser =  createAsyncThunk(
+  'login',
+  async (loginDetails, thunkAPI) => {
+    try {
+      const response = await axios.post(
+        '/api/v1/login',
+        {loginDetails},
+        {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+      
+      console.log(response.data)
+      return response.data;
+
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.response.data)
+    }
+  }
+);
+
+const initialState = {
+  isLoading: false,
+  user: null,
+  authenticated: false,
+};
+
+const userSlice = createSlice({
+  name: user,
+  initialState,
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(createUser.pending, (state) => {state.isLoading = true})
+      .addCase(createUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = action.payload;
+        state.authenticated = true;
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.authenticated = false;
+      })
+      .addCase(logInUser.pending, (state) => {state.isLoading = true})
+      .addCase(logInUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = action.payload;
+        state.authenticated = true;
+      })
+      .addCase(logInUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.authenticated = false;
+      })
+  }
+});
+
+export { logInUser };
+export default userSlice.reducer;
