3fd4839a0a3f0a646c4e09858c9cd917d018e842
stephenkati <stephenkatuli88@gmail.com>
2023-07-23 15:59:07 +0300

    Setup and style Login and Registration

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -24,29 +24,41 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form id="form">
+    <div className="flex flex-col gap-4 bg-neutral p-4 text-primary rounded-lg shadow-lg">
+      <h1 className="text-3xl text-secondary font-bold">Login</h1>
+      <form id="form" className="flex flex-col gap-2 w-full">
         <input
           type="email"
           placeholder="Email"
           onChange={(e) => setEmail(e.target.value)}
+          className="text-secondary-focus w-full py-2 px-4 rounded-full bg-accent border-none shadow-none outline-none focus:ring focus:ring-secondary placeholder-neutral-focus"
         />
         <input
           type="password"
           placeholder="Password"
           onChange={(e) => setPassword(e.target.value)}
+          className="text-secondary-focus w-full py-2 px-4 rounded-full bg-accent border-none shadow-none outline-none focus:ring focus:ring-secondary placeholder-neutral-focus"
         />
         {isLoading ? (
-          <p>Loading...</p>
+          <p className="text-lg text-secondary">Loading...</p>
         ) : (
-          <button type="button" onClick={handleLogin} disabled={isLoading}>
+          <button
+            type="button"
+            onClick={handleLogin}
+            disabled={isLoading}
+            className="mt-4 w-full p-2 rounded-full bg-secondary text-primary font-bold hover:bg-accent-focus hover:text-secondary"
+          >
             Login
           </button>
         )}
       </form>
-      <h2>
-        Don't have an account? <Link to="/register">Register</Link>
+      <h2 className="text-lg">
+        Don't have an account?
+        <Link to="/register"
+          className="text-secondary font-bold hover:text-primary px-2 underline decoration-white underline-offset-8"
+        >
+          Register
+        </Link>
       </h2>
     </div>
   );
--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom'; // Import Link from react-router-dom
 import { createUser } from '../redux/userReducer';
 
@@ -9,6 +9,7 @@ const Register = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { isLoading } = useSelector((state) => state.user);
 
   const registrationDetails = { name, email, password };
 
@@ -24,31 +25,48 @@ const Register = () => {
   };
 
   return (
-    <div>
-      <h1>Register</h1>
+    <div className="flex flex-col gap-4 bg-neutral p-4 text-primary rounded-lg shadow-lg">
+      <h1 className="text-3xl text-secondary font-bold">Register</h1>
 
-      <form id="form">
+      <form id="form" className="flex flex-col gap-2 w-full">
         <input
           type="text"
           placeholder="Name"
           onChange={(e) => setName(e.target.value)}
+          className="text-secondary-focus w-full py-2 px-4 rounded-full bg-accent border-none shadow-none outline-none focus:ring focus:ring-secondary placeholder-neutral-focus"
         />
         <input
           type="email"
           placeholder="Email"
           onChange={(e) => setEmail(e.target.value)}
+          className="text-secondary-focus w-full py-2 px-4 rounded-full bg-accent border-none shadow-none outline-none focus:ring focus:ring-secondary placeholder-neutral-focus"
         />
         <input
           type="password"
           placeholder="Password"
           onChange={(e) => setPassword(e.target.value)}
+          className="text-secondary-focus w-full py-2 px-4 rounded-full bg-accent border-none shadow-none outline-none focus:ring focus:ring-secondary placeholder-neutral-focus"
         />
-        <button type="button" onClick={handleRegister}>
-          Register
-        </button>
+        {isLoading ? (
+          <p className="text-lg text-secondary">Loading...</p>
+        ) : (
+          <button
+            type="button"
+            onClick={handleRegister}
+            className="mt-4 w-full p-2 rounded-full bg-secondary text-primary font-bold hover:bg-accent-focus hover:text-secondary"
+          >
+            Register
+          </button>
+        )}
       </form>
 
-      <h2>Already have an account! <Link to="/login">Login</Link></h2>
+      <h2 className="text-lg">
+        Already have an account!
+        <Link
+          to="/login"
+          className="text-secondary font-bold hover:text-primary px-2 underline decoration-white underline-offset-8"
+        >Login</Link>
+      </h2>
     </div>
   );
 };
