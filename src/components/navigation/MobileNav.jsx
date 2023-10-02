f03cbf3d2e0e6ae0da5c55560ed797df127f5896
stephenkati <stephenkatuli88@gmail.com>
2023-10-02 16:46:44 +0300

    Setup mobile menu

--- src/components/navigation/MobileNav.jsx
+++ src/components/navigation/MobileNav.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import { BsPersonBoundingBox } from 'react-icons/bs';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
+import { FaBars } from 'react-icons/fa';
 
 const links = [
   { path: '/', text: 'Home' },
@@ -7,23 +11,48 @@ const links = [
 ];
 
 const MobileNav = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  };
 
   return (
     <div className="md:hidden">
-      {/* <ul className="flex flex-col gap-4">
-        {
-          links.map((link) => (
-            <li key={link.path}>
-              <Link
-                to={link.path}
-                className="slide-in"
-              >
-                {link.text}
-              </Link>
-            </li>
-          ))
-        }
-      </ul> */}
+      
+      <button
+        className="text-3xl absolute top-4 right-4 close"
+        onClick={handleToggleMenu}
+      >
+        <FaBars />
+      </button>
+
+      {isMenuOpen &&
+        <div className="bg-secondary absolute top-0 right-0 left-0 p-5">
+          <div className="flex justify-between w-full py-4 text-3xl ">
+            <BsPersonBoundingBox className="profile" />
+            <AiOutlineCloseCircle
+              className="close"
+              onClick={handleToggleMenu}
+            />
+          </div>
+          <ul className="flex flex-col gap-4">
+            {
+              links.map((link) => (
+                <li key={link.path}>
+                  <Link
+                    to={link.path}
+                    className="slide-in"
+                  >
+                    {link.text}
+                  </Link>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+      }
+
     </div>
   )
 }
