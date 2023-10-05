d90969c5f6c7ef0f2ae7647e193049defd411236
stephenkati <stephenkatuli88@gmail.com>
2023-10-05 21:59:08 +0300

    Add Services

---
+++ src/components/Services/ServiceList.jsx
@@ -0,0 +1,32 @@
+export const ServiceList = [
+  {
+    image: 'img',
+    title: 'Dry Cleaning',
+    description: 'Our dry cleaning service offers a convenient, professional way to get your clothes clean and looking fresh.'
+  },
+  {
+    image: 'img',
+    title: 'Stain Removal',
+    description: 'Our stain removal service quickly and effectively removes the toughest stains from your clothes.'
+  },
+  {
+    image: 'img',
+    title: 'Ironing and Pressing',
+    description: 'We offer professional ironing and pressing services to give your garments that crisp, clean look.'
+  },
+  {
+    image: 'img',
+    title: 'Garment Alterations',
+    description: 'We offer quick and accurate garment alterations to make sure your clothes fit perfectly.'
+  },
+  {
+    image: 'img',
+    title: 'Folding and Packaging',
+    description: 'We offer fast and convenient laundry folding and packaging services for your convenience.'
+  },
+  {
+    image: 'img',
+    title: 'Shoe Cleaning',
+    description: 'Our shoe cleaning service will restore your footwear to like-new condition with our professional cleaning techniques.'
+  }
+];
