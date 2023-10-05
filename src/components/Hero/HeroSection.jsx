63e34288ca577e3c58910ab3b7493f1e72ace45d
stephenkati <stephenkatuli88@gmail.com>
2023-10-05 21:59:33 +0300

    Update Herosection

--- src/components/Hero/HeroSection.jsx
+++ src/components/Hero/HeroSection.jsx
@@ -3,7 +3,7 @@ import HeroContent from './HeroContent';
 
 const HeroSection = () => {
   return (
-    <div className='mt-16 py-2 px-10 hero-sect relative'>
+    <div className='mt-16 py-2 px-10 hero-sect relative bg-accent'>
       <HeroContent />
     </div>
   )
