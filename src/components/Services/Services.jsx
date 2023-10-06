cb52dad97ec2152f6fd9176104bc9d2ea803c15a
stephenkati <stephenkatuli88@gmail.com>
2023-10-06 13:29:26 +0300

    Add images

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -9,11 +9,11 @@ const Services = () => {
 
       <div className="flex flex-wrap gap-4 w-full justify-center p-7">
         {ServiceList.map((service, index) => (
-          <div key={index} className="p-4 w-64 text-center items-center min-h-max my-5 bg-accent">
+          <div key={index} className="p-4 w-64 text-center items-center h-fit my-5">
             <div className='w-full h-2/3'>
-              <img src={service.image} alt={service.title} className='w-full' />
+              <img src={service.image} alt={service.title} className='w-full h-40 rounded-lg' />
             </div>
-            <h5 className="text-xl font-semibold pb-10">{service.title}</h5>
+            <h5 className="text-xl text-secondary font-semibold py-5">{service.title}</h5>
             <p>{service.description}</p>
           </div>
         ))}
